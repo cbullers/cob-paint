42db346e09032a90b5025c95c42439911703bf19
Chris Bullers <chrisbullers@gmail.com>
2016-04-06 10:55:03 -0500

    Update COBPaint.java

--- COBPaint.java
+++ COBPaint.java
@@ -1,11 +1,24 @@
 package com.cob.paint;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JApplet;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.SwingUtilities;
@@ -27,11 +40,11 @@ public class COBPaint extends JApplet {
 	public void init() {
 		
 		setSize(1000,650);
+		final Application app = new Application(this);
 		
 		try {
 			SwingUtilities.invokeAndWait(new Runnable() {
 				public void run() {
-					Application app = new Application();
 					setContentPane(app);
 				}
 			});
@@ -49,12 +62,126 @@ class Application extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private static Dimension SLIDER_DIMENSION = new Dimension(30,500);
 	private int curR, curB, curG = 255;
+	private URL codeBase;
+	public Tool currentTool;
 	
-	public Application() {
+	JSlider redSlider;
+	JSlider greenSlider;
+	JSlider blueSlider;
+	
+	JButton penButton;
+	JButton rollerButton;
+	
+	JPanel canvas;
+	
+	Toolkit toolkit = Toolkit.getDefaultToolkit();
+	Image penCursorImg = toolkit.getImage("../pencil.png");
+	Cursor penCursor = toolkit.createCustomCursor(penCursorImg, new Point(this.getX(), this.getY()), "penCursor");
+	
+	Image rollerCursorImg = toolkit.getImage("../roller.png");
+	Cursor rollerCursor = toolkit.createCustomCursor(rollerCursorImg, new Point(this.getX(), this.getY()), "rollerCursor");
+	
+	public Application(JApplet a) { 
+		codeBase = a.getCodeBase();
+		setLayout(null);
+		initSliders();
+		initCanvas();
+		initButtons();
+		add(redSlider);
+		add(greenSlider);
+		add(blueSlider);
+		add(penButton);
+		add(rollerButton);
+		add(canvas);
+	}
+	
+	private void repaintIt(){
+		super.repaint();
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		drawMenu(g);
+	}
+	
+	public void drawMenu(Graphics g) {
 		
-		JSlider redSlider = new JSlider(JSlider.VERTICAL, 0, 255, 255);
-		JSlider greenSlider = new JSlider(JSlider.VERTICAL, 0, 255, 255);
-		JSlider blueSlider = new JSlider(JSlider.VERTICAL, 0, 255, 255);
+		g.setColor(new Color(187,187,187));
+		g.fillRect(0, 0, 1000, 50);
+		
+		g.fillRect(800, 25, 25, 625);
+		
+		g.setColor(Color.BLACK);
+		g.drawString("RED", 850, 575);
+		g.drawString("GREEN", 890, 575);
+		g.drawString("BLUE", 950, 575);
+		
+		g.setColor(new Color(curR, curG, curB));
+		g.fillRect(835,595,150,35);
+		
+		String hexValue = String.format("#%02X%02X%02X", curR, curG, curB);
+		g.setColor(Color.black);
+		g.drawString("Hex Value: " + hexValue, 850, 617);
+		
+		// Draw the pen tool image
+
+		
+		// Draw the roller tool image
+		
+	}
+	
+	private void initButtons() {
+		// Pen button
+		penButton = new JButton();
+		penButton.setSize(32,32);
+		penButton.setLocation(7,7);
+		try{
+			URL penUrl = new URL(codeBase, "../pencil.png");
+			Image img = ImageIO.read(penUrl);
+			img = img.getScaledInstance(32, 32, Image.SCALE_DEFAULT);
+			penButton.setIcon(new ImageIcon(img));
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		penButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent a) {
+				currentTool = new PenTool();
+				resetCursor();
+			}
+			
+		});
+		
+		
+		// Roller Button
+		rollerButton = new JButton();
+		rollerButton.setSize(32,32);
+		rollerButton.setLocation(45,7);
+		try{
+			URL rollerUrl = new URL(codeBase, "../roller.png");
+			Image img = ImageIO.read(rollerUrl);
+			img = img.getScaledInstance(32, 32, Image.SCALE_DEFAULT);
+			rollerButton.setIcon(new ImageIcon(img));
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		rollerButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				currentTool = new RollerTool();
+			}
+			
+		});
+		
+	}
+	
+	public void initSliders() {
+		redSlider = new JSlider(JSlider.VERTICAL, 0, 255, 255);
+		greenSlider = new JSlider(JSlider.VERTICAL, 0, 255, 255);
+		blueSlider = new JSlider(JSlider.VERTICAL, 0, 255, 255);
 		
 		redSlider.setSize(SLIDER_DIMENSION);
 		greenSlider.setSize(SLIDER_DIMENSION);
@@ -98,42 +225,80 @@ class Application extends JPanel {
 			}
 		});
 		
-		setLayout(null);
-		
-		add(redSlider);
-		add(greenSlider);
-		add(blueSlider);
-		
 	}
 	
-	private void repaintIt(){
-		super.repaint();
+	private void resetCursor() {
+		if(currentTool instanceof PenTool) {
+			this.setCursor(penCursor);
+		}else if(currentTool instanceof RollerTool) {
+			this.setCursor(rollerCursor);
+		}
 	}
+	
+	private void setDefaultCursor() {
+		this.setCursor(Cursor.getDefaultCursor());
+	}
+	
+	private void initCanvas() {
+		
+		boolean pressingDown = false;
+		
+		canvas = new JPanel();
+		canvas.setLocation(0,50);
+		canvas.setSize(800,600);
+		canvas.addMouseListener(new MouseListener() {
 
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		drawMenu(g);
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				System.out.println("hello");
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				resetCursor();
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				setDefaultCursor();
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				pressingDown = true;
+				canvasLogic(); // Need to make
+			}
+
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				pressingDown = false;
+			}
+			
+		});
+	}
+}
+
+class Tool {
+	
+	public int brushSize = 5;
+	
+	public void setBrushSize(int s) {
+		this.brushSize = s;
 	}
 	
-	public void drawMenu(Graphics g) {
-		
-		g.setColor(new Color(187,187,187));
-		g.fillRect(0, 25, 1000, 25);
-		
-		g.fillRect(800, 25, 25, 625);
-		
-		g.setColor(Color.BLACK);
-		g.drawString("RED", 850, 575);
-		g.drawString("GREEN", 890, 575);
-		g.drawString("BLUE", 950, 575);
-		
-		g.setColor(new Color(curR, curG, curB));
-		g.fillRect(835,595,150,35);
-		
-		String hexValue = String.format("#%02X%02X%02X", curR, curG, curB);
-		g.setColor(Color.black);
-		g.drawString("Hex Value: " + hexValue, 850, 617);
-	}
+}
+
+class PenTool extends Tool {
+	
+}
+
+class RollerTool extends Tool {
+	
+}
+
+class BucketTool extends Tool {
+	
 	
 }
